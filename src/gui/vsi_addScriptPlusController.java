facaf706cdd44823642ebb68f863cfc1d8dd7e1d
tassi <tassi@DESKTOP-H09PK5M>
2019-12-09 01:04:40 -0200

    Desenhada nova tela ADD+, ainda não adicionada no fluxo da interface.

---
+++ src/gui/vsi_addScriptPlusController.java
@@ -0,0 +1,72 @@
+package gui;
+
+import com.jfoenix.controls.JFXListView;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class vsi_addScriptPlusController {
+
+    @FXML
+    private JFXListView<?> lvScriptPreview;
+    @FXML
+    private JFXListView<?> lvFunctionsList;
+    
+    
+    
+    @FXML
+    void clickAddScript(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickDeleteScript(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickDoubleScript(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickIndDown(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickIndLeft(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickIndRight(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickIndUp(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickMoveScriptLeft(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickMoveScriptRight(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickNewCondScript(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickNewSimpleScript(ActionEvent event) {
+
+    }
+
+}
